1c3daac6b298d18f90eeea213578844670c9341c
stoneharry <stoneharryjames@gmail.com>
2018-03-11 21:06:00 +0000

    Include ItemRange class update for untested inventoryType generation

--- tools/ItemDBCGenerator/ItemDBCGenerator/ItemDBCGenerator/ItemRange.cs
+++ tools/ItemDBCGenerator/ItemDBCGenerator/ItemDBCGenerator/ItemRange.cs
@@ -12,13 +12,25 @@ namespace ItemDBCGenerator
         public Constants.ItemClass itemClass { get; set; }
         public Constants.WeaponSubclass weaponSubClass { get; set; }
         public Constants.ArmorSubclass armorSubClass { get; set; }
+        public Constants.InventoryType inventoryType { get; set; }
+        public bool containsInventoryType { get; } = false;
 
-        public ItemRange(UInt32[] range, Constants.ItemClass itemClass, Constants.ArmorSubclass armorSubclass)
+        public ItemRange(UInt32[] range, Constants.ItemClass itemClass, Constants.ArmorSubclass armorSubClass)
         {
             this.range = range;
             this.itemClass = itemClass;
-            this.armorSubClass = armorSubclass;
-            this.weaponSubClass = Constants.WeaponSubclass.NULL;
+            this.armorSubClass = armorSubClass;
+            weaponSubClass = Constants.WeaponSubclass.NULL;
+        }
+
+        public ItemRange(UInt32[] range, Constants.ItemClass itemClass, Constants.ArmorSubclass armorSubClass, Constants.InventoryType inventoryType)
+        {
+            this.range = range;
+            this.itemClass = itemClass;
+            this.armorSubClass = armorSubClass;
+            this.inventoryType = inventoryType;
+            weaponSubClass = Constants.WeaponSubclass.NULL;
+            containsInventoryType = true;
         }
 
         public ItemRange(UInt32[] range, Constants.ItemClass itemClass, Constants.WeaponSubclass weaponSubClass)
@@ -26,7 +38,7 @@ namespace ItemDBCGenerator
             this.range = range;
             this.itemClass = itemClass;
             this.weaponSubClass = weaponSubClass;
-            this.armorSubClass = Constants.ArmorSubclass.NULL;
+            armorSubClass = Constants.ArmorSubclass.NULL;
         }
     }
 }
